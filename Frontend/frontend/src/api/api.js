89eba5c9529a5cd8273a36ebb348c824ac6a51ab
fahd2001 <mammoufahd@gmail.com>
2026-01-21 11:25:09 -0500

    Change API base URL to localhost for development

--- Frontend/frontend/src/api/api.js
+++ Frontend/frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tp2-programmation-web-avance-fahd-et.onrender.com",
+  baseURL: "http://localhost:3000",
 });
 
 export default api;
